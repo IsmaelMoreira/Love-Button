5e06eb654b6fe21aa09a5fe561b9e5f230caaf49
ismael <moreiraismael14@gmail.com>
2024-06-08 23:48:11 -0300

    js config

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 let button = document.getElementById('no');
 
-button.addEventListener('mouseover', function(){
+button.addEventListener('mouseover', function() {
     let height = window.innerHeight - button.offsetHeight;
     let width = window.innerWidth - button.offsetWidth;
     
     button.style.position = "absolute";
     button.style.top = Math.random() * height + "px";
     button.style.left = Math.random() * width + "px";
+});
+
+button.addEventListener('click', function() {
+    alert("Você não pode dizer não! Por favor, escolha 'Sim'!");
 });
